9f44ecd0f958f7d412b8dd413e486aff32729058
Alunos Laboratorio <aluno.laboratorio@faesa.br>
2024-03-11 21:25:02 -0300

    Hidding token

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
+console.log(process.env)
 const TelegramBot = require('node-telegram-bot-api');
 
-const token = 'xxxxxx';
+const token = process.env.TOKEN;
 
 const bot = new TelegramBot(token, {polling: true});
 
@@ -15,5 +17,5 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
 
-  bot.sendMessage(chatId, 'Olá, acabo de receber sua mensagem!');
+  bot.sendMessage(chatId, 'Olá, acabo de receber sua mensagem! bbb');
 });
